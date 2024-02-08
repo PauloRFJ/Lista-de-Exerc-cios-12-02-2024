0e766bd163232bf5ea43bebd4b4e825c126d7177
Roberto <roberto220488@gmail.com>
2024-02-08 19:46:37 -0300

    Exercício 2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // // Recebe a idade da pessoa
 
-// var idade = prompt("Digite a sua idade:");
+// let idade = prompt("Digite a sua idade:");
 
 // // Verifica se a idade é maior ou igual a 18
 
@@ -15,10 +15,7 @@
       // Exercício 2
 
 // Recebe o número
-var numero = prompt("Digite um número:");
-
-// Converte o número para um valor numérico
-numero = Number(numero);
+let numero = prompt("Digite um número:");
 
 // Verifica se o número é positivo, negativo ou zero
 if (numero > 0) {
