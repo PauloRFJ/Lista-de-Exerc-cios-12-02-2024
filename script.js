c52a3930d3dbbbb39d673f3b9f9734aa705fe71c
Roberto <roberto220488@gmail.com>
2024-02-08 19:27:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Recebe a idade da pessoa
+var idade = prompt("Digite a sua idade:");
+
+// Verifica se a idade é maior ou igual a 18
+if (idade >= 18) {
+  console.log("Você é maior de idade.");
+} else {
+  console.log("Você é menor de idade.");
+}
