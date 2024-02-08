4da3f6f37320bdffff252348b2aec6572b516b1c
Roberto <roberto220488@gmail.com>
2024-02-08 20:05:27 -0300

    Exercícios comentados

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-      //Exercício 1
+                             //LISTA DE EXERCÍCIOS 08-02-2024
+     
+     
+     //Exercício 1 - Saber se a pessoa é maior ou menor de idade.
 
 // // Recebe a idade da pessoa
 
@@ -12,16 +15,48 @@
 //   console.log("Você é menor de idade.");
 // }
 
-      // Exercício 2
 
-// Recebe o número
-let numero = prompt("Digite um número:");
 
-// Verifica se o número é positivo, negativo ou zero
-if (numero > 0) {
-  console.log("O número é positivo.");
-} else if (numero < 0) {
-  console.log("O número é negativo.");
-} else {
-  console.log("O número é zero.");
-}
+
+//       // Exercício 2 - Saber se um número é positivo ou não.
+
+// // Recebe o número
+// let numero = prompt("Digite um número:");
+
+// // Verifica se o número é positivo, negativo ou zero
+// if (numero > 0) {
+//   console.log("O número é positivo.");
+// } else if (numero < 0) {
+//   console.log("O número é negativo.");
+// } else {
+//   console.log("O número é zero.");
+// }
+
+
+
+//      //Exercício 3 - Saber se o ano é bissexto ou não.
+
+// // Recebe o ano
+// let ano = parceInt(prompt("Digite um ano:"));
+
+// // Verifica se o ano é bissexto
+// if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
+//   console.log("O ano é bissexto.");
+// } else {
+//   console.log("O ano não é bissexto.");
+// }
+
+
+
+
+
+//         // Exercício 4 - Saber se um número é par ou impar.
+
+// let numero = parseInt(prompt("Digite um número:"));
+ 
+//    if (numero % 2 === 0) {
+//       console.log("O número é par.");
+//    } else {
+//       console.log("O número é ímpar.");
+//    }
+        
