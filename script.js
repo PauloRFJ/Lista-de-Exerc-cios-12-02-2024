28cb560608382cf89f19c5095cf648badea5c23d
Roberto <roberto220488@gmail.com>
2024-02-08 19:42:40 -0300

    Exercício 2

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-// Recebe a idade da pessoa
-var idade = prompt("Digite a sua idade:");
+      //Exercício 1
 
-// Verifica se a idade é maior ou igual a 18
-if (idade >= 18) {
-  console.log("Você é maior de idade.");
+// // Recebe a idade da pessoa
+
+// var idade = prompt("Digite a sua idade:");
+
+// // Verifica se a idade é maior ou igual a 18
+
+// if (idade >= 18) {
+//   console.log("Você é maior de idade.");
+// } else {
+//   console.log("Você é menor de idade.");
+// }
+
+      // Exercício 2
+
+// Recebe o número
+var numero = prompt("Digite um número:");
+
+// Converte o número para um valor numérico
+numero = Number(numero);
+
+// Verifica se o número é positivo, negativo ou zero
+if (numero > 0) {
+  console.log("O número é positivo.");
+} else if (numero < 0) {
+  console.log("O número é negativo.");
 } else {
-  console.log("Você é menor de idade.");
+  console.log("O número é zero.");
 }
