e1d4c85a7e89733758af06566d4299ae78056ce2
Roberto <roberto220488@gmail.com>
2024-02-12 23:37:49 -0300

    LISTA DE EXERCÍCIOS 12-02-2024

--- script.js
+++ script.js
@@ -1,324 +1,215 @@
-                             //LISTA DE EXERCÍCIOS 08-02-2024
-     
+                         //LISTA DE EXERCÍCIOS 12-02-2024  -  Paulo Roberto
 
-        // If e Else
-     
-     // Exercício 1 - Saber se a pessoa é maior ou menor de idade.
+                         
+ 
+ // 10- Crie uma função que solicite para o usuário através do prompt de comando sua idade, e a partir da idade fornecida, 
+ //exiba uma mensagem informando se ele é maior de idade ou não. Considere 18 anos como maior idade.
+ 
+//  let idade = prompt("Digite a sua idade:");
 
-// // Recebe a idade da pessoa
-
-// let idade = prompt("Digite a sua idade:");
-
-// // Verifica se a idade é maior ou igual a 18
-
-// if (idade >= 18) {
+//  if (idade >= 18) {
 //   console.log("Você é maior de idade.");
 // } else {
 //   console.log("Você é menor de idade.");
 // }
-
-
-
-
-//       // Exercício 2 - Saber se um número é positivo ou não.
-
-// // Recebe o número
-// let numero = prompt("Digite um número:");
-
-// // Verifica se o número é positivo, negativo ou zero
-// if (numero > 0) {
-//   console.log("O número é positivo.");
-// } else if (numero < 0) {
-//   console.log("O número é negativo.");
-// } else {
-//   console.log("O número é zero.");
-// }
-
-
-
-//      //Exercício 3 - Saber se o ano é bissexto ou não.
-
-// // Recebe o ano
-// let ano = parceInt(prompt("Digite um ano:"));
-
-// // Verifica se o ano é bissexto
-// if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
-//   console.log("O ano é bissexto.");
-// } else {
-//   console.log("O ano não é bissexto.");
-// }
-
-
-
-
-
-//         // Exercício 4 - Saber se um número é par ou impar.
-
-// let numero = parseInt(prompt("Digite um número:"));
  
-//    if (numero % 2 === 0) {
-//       console.log("O número é par.");
-//    } else {
-//       console.log("O número é ímpar.");
-//    }
-        
 
 
-
-
-           // Exercício 5 - Saber se o aluno foi aprovado, sendo que >=7 Aprovado  >=5 e >=7 recuperação e <5 reprovado
  
-  //  let nota = parseFloat(prompt("Digite a nota do aluno:"));
  
-  //     if (nota >= 7) {
-  //       console.log("Aluno aprovado.");
-  //     } else if (nota >= 5) {
-  //        console.log("Aluno em recuperação.");
-  //     } else {
-  //         console.log("Aluno reprovado.");
-  //     }
-
-
-
-
-
-      //      // Exercício 6 - Faça um programa que determine o maior de três números informados.
-
-      // let num1 = parseFloat(prompt("Digite o primeiro número:"));
-      // let num2 = parseFloat(prompt("Digite o segundo número:"));
-      // let num3 = parseFloat(prompt("Digite o terceiro número:"));
-            
-      //   if (num1 > num2 && num1 > num3) {
-      //     console.log("O primeiro número é o maior.");
-      //   } else if (num2 > num1 && num2 > num3) {
-      //       console.log("O segundo número é o maior.");
-      //   } else {
-      //      console.log("O terceiro número é o maior.");
-      //   }
-                     
-
-
-
-
-
-      //     // Exercício 7 - verifique se um ano é par ou ímpar.
-
-      // let ano = parseInt(prompt("Digite um ano:"));
+ // 9- Use um switch para exibir mensagens diferentes com base em uma condição.
  
-      //   if (ano % 2 === 0) {
-      //      console.log("O ano é par.");
-      // } else {
-      //      console.log("O ano é ímpar.");
-      // }
-
-  
-
-             // Switch e Case
-
-
-      //         // Exercício 1 - Escreva um programa que leia um número de 1 a 7 e imprima o dia da semana correspondente.
- 
-      // let numero = parseInt(prompt("Digite um número de 1 a 7:"));
+//  let numero = parseInt(prompt("Digite um número de 1 a 7:"));
        
-      // switch(numero) {
-      //     case 1:
-      //         console.log("Domingo");
-      //         break;
-      //     case 2:
-      //         console.log("Segunda-feira");
-      //         break;
-      //     case 3:
-      //         console.log("Terça-feira");
-      //         break;
-      //     case 4:
-      //         console.log("Quarta-feira");
-      //         break;
-      //     case 5:
-      //         console.log("Quinta-feira");
-      //         break;
-      //     case 6:
-      //         console.log("Sexta-feira");
-      //         break;
-      //     case 7:
-      //         console.log("Sábado");
-      //         break;
-      //     default:
-      //         console.log("Número inválido");
-      // }
-      
+//       switch(numero) {
+//           case 1:
+//               console.log("Domingo");
+//               break;
+//           case 2:
+//               console.log("Segunda-feira");
+//               break;
+//           case 3:
+//               console.log("Terça-feira");
+//               break;
+//           case 4:
+//               console.log("Quarta-feira");
+//               break;
+//           case 5:
+//               console.log("Quinta-feira");
+//               break;
+//           case 6:
+//               console.log("Sexta-feira");
+//               break;
+//           case 7:
+//               console.log("Sábado");
+//               break;
+//           default:
+//               console.log("Número inválido");
+//       }                            
+                             
+                 
+
+
+
+                             
+
+// 8- Implemente uma função que determine se um número é par ou ímpar.
+
+// function verificarParOuImpar(numero) {
+//        if (numero % 2 === 0) {
+//               console.log("O número é par");
+//        } else {
+//               console.log("O número é ímpar");
+//        }
+// }
+
+// verificarParOuImpar(4); // Par
+// verificarParOuImpar(7); // Ímpar
 
 
 
 
-          //       // Exercício 2 - Crie um programa que verifique se uma letra é vogal ou consoante.
-
-          // let letra = prompt("Digite uma letra:").toLowerCase();
- 
-          //   switch(letra) {
-          //      case 'a':
-          //      case 'e':
-          //      case 'i':
-          //      case 'o':
-          //      case 'u':
-          //        console.log("A letra é uma vogal.");
-          //           break;
-          //       default:
-          //        console.log("A letra é uma consoante.");
-          //  }
-                
 
 
 
-            //        // Exercício 3 - Faça um programa que converta um número em um mês do ano.
+// 7- Crie uma função que receba o valor de uma hora qualquer,
+//e exibe no console uma mensagem de “bom dia”, “boa tarde” ou “boa noite”, de acordo com a hora passada.
 
-            //  let numero = parseInt(prompt("Digite um número de 1 a 12:"));
- 
-            //    switch(numero) {
-            //      case 1:
-            //        console.log("Janeiro");
-            //          break;
-            //      case 2:
-            //        console.log("Fevereiro");
-            //          break;
-            //       case 3:
-            //        console.log("Março");
-            //          break;
-            //       case 4:
-            //        console.log("Abril");
-            //          break;
-            //       case 5:
-            //        console.log("Maio");
-            //          break;
-            //       case 6:
-            //        console.log("Junho");
-            //          break;
-            //       case 7:
-            //         console.log("Julho");
-            //          break;
-            //       case 8:
-            //         console.log("Agosto");
-            //          break;
-            //       case 9:
-            //         console.log("Setembro");
-            //          break;
-            //       case 10:
-            //         console.log("Outubro");
-            //          break;
-            //       case 11:
-            //         console.log("Novembro");
-            //          break;
-            //       case 12:
-            //         console.log("Dezembro");
-            //          break;
-            //        default:
-            //         console.log("Número inválido");
-            //     }
-                                    
+// function saudacaoPorHora(hora) {
+//        if (hora >= 0 && hora < 12) {
+//               console.log("Bom dia!");
+//        } else if (hora >= 12 && hora < 18) {
+//               console.log("Boa tarde!");
+//        } else if (hora > 18 && hora < 24) {
+//               console.log("Boa noite!");
+//        } else
+//               console.log("Hora Inválida")
+// }
 
-
-
-              //        // Exercício 4 - Crie um programa que classifique um dia como útil ou final de semana.
-
-              //  let dia = parseInt(prompt("Digite o número do dia (1 a 7):"));
- 
-              //     switch(dia) {
-              //       case 1:
-              //       case 2:
-              //       case 3:
-              //       case 4:
-              //       case 5:
-              //         console.log("Dia útil.");
-              //           break;
-              //       case 6:
-              //       case 7:
-              //         console.log("Final de semana.");
-              //           break;
-              //        default:
-              //         console.log("Dia inválido.");
-              //    }
+// saudacaoPorHora(6); // Exemplo "bom dia"
+// saudacaoPorHora(17); // Exemplo "boa tarde"
+// saudacaoPorHora(23); // Exemplo "boa noite"
+// saudacaoPorHora(65); // Exemplo "hora inválida"
 
 
 
 
-                      
-                        // Exercício 5 - Escreva um programa que determine o nome de um polígono a partir do número de lados.
-
-                // let lados = parseInt(prompt("Digite o número de lados do polígono:"));
- 
-                //   switch(lados) {
-                //     case 3:
-                //      console.log("Triângulo");
-                //        break;
-                //     case 4:
-                //      console.log("Quadrilátero");
-                //        break;
-                //     case 5:
-                //      console.log("Pentágono");
-                //        break;
-                //     case 6:
-                //      console.log("Hexágono");
-                //        break;
-                //     case 7:
-                //      console.log("Heptágono");
-                //        break;
-                //     case 8:
-                //      console.log("Octógono");
-                //        break;
-                //       default:
-                //      console.log("Polígono não identificado");
-                //  }
-                               
-                
 
 
+// 6- Crie 3 objetos com propriedades representando informações pessoais (nome, idade, nacionalidade e profissão) e
+//exiba essas informações com um console.log para cada objeto.
 
+// let pessoa1 = {
+//        nome: "Alex",
+//        idade: 20,
+//        nacionalidade: "Italiano",
+//        profissao: "Professor"
+// };
 
-                       // Exercício 6 - Faça um programa que verifique se um número é positivo, negativo ou zero.
+// let pessoa2 = {
+//        nome: "Helena",
+//        idade: 32,
+//        nacionalidade: "Brasileira",
+//        profissao: "Médica"
+// };
 
-                      //  let numero = parseFloat(prompt("Digite um número:"));
- 
-                      //  switch(true) {
-                      //      case numero > 0:
-                      //          console.log("O número é positivo.");
-                      //          break;
-                      //      case numero < 0:
-                      //          console.log("O número é negativo.");
-                      //          break;
-                      //      default:
-                      //          console.log("O número é zero.");
-                      //  }
-                                            
-                                        
+// let pessoa3 = {
+//        nome: "Eduardo",
+//        idade: 52,
+//        nacionalidade: "Chileno",
+//        profissao: "Policial"
+// };
+
+// console.log("Informações da pessoa 1:");
+// console.log("Nome: " + pessoa1.nome);
+// console.log("Idade: " + pessoa1.idade);
+// console.log("Nacionalidade: " + pessoa1.nacionalidade);
+// console.log("Profissão: " + pessoa1.profissao);
+
+// console.log("Informações da pessoa 2:");
+// console.log("Nome: " + pessoa2.nome);
+// console.log("Idade: " + pessoa2.idade);
+// console.log("Nacionalidade: " + pessoa2.nacionalidade);
+// console.log("Profissão: " + pessoa2.profissao);
+
+// console.log("Informações da pessoa 3:");
+// console.log("Nome: " + pessoa3.nome);
+// console.log("Idade: " + pessoa3.idade);
+// console.log("Nacionalidade: " + pessoa3.nacionalidade);
+// console.log("Profissão: " + pessoa3.profissao);
 
 
 
 
-                       // Exercício 7 - Crie um programa que determine a estação do ano a partir do mês.
 
-                      //  let mes = parseInt(prompt("Digite o número do mês (1 a 12):"));
- 
-                      //  switch(mes) {
-                      //      case 12:
-                      //      case 1:
-                      //      case 2:
-                      //          console.log("Verão");
-                      //          break;
-                      //      case 3:
-                      //      case 4:
-                      //      case 5:
-                      //          console.log("Outono");
-                      //          break;
-                      //      case 6:
-                      //      case 7:
-                      //      case 8:
-                      //          console.log("Inverno");
-                      //          break;
-                      //      case 9:
-                      //      case 10:
-                      //      case 11:
-                      //          console.log("Primavera");
-                      //          break;
-                      //      default:
-                      //          console.log("Mês inválido");
-                      //  }
-                       
+
+// 5- Utilize uma estrutura condicional (if,else) para verificar se um número é positivo, negativo ou zero.
+//    Exiba os valores no console.log().
+
+// function verificarNumero(numero) {
+//        if (numero > 0) {
+//               console.log("O número é positivo");
+//        } else if (numero < 0) {
+//               console.log("O número é negativo");
+//        } else {
+//               console.log("O número é zero");
+//        }
+// }
+
+// verificarNumero(10); // número positivo
+// verificarNumero(-10); // número negativo
+// verificarNumero(0); // zero
+
+
+
+
+
+
+// 4- Crie uma função que receba dois números como parâmetros e retorne sua soma no console.log.
+
+// function somarNumeros(num1, num2) {
+//        let soma = num1 + num2;
+//        console.log("A soma dos números é: " + soma);
+// }
+
+// somarNumeros(35, 15);
+
+
+
+
+
+
+// 3- Combine duas strings usando o operador de concatenação e exiba o resultado no console.
+//      Utilize o template string ou a concatenação simples.
+
+// let string1 = "Turma";
+// let string2 = "Fitness";
+
+// // Possibilidade 1
+// let resultado1 = string1 + " " + string2;
+// console.log(resultado1);
+
+// // Possibilidade 2
+// let resultado2 = `${string1} ${string2}`;
+// console.log(resultado2);
+
+
+
+
+
+// 2- Declare uma variável e atribua um número a ela, em seguida, exiba seu valor no console.
+// Atribua o valor a variável usando o window.prompt();
+
+// let numero = window.prompt("Digite um número:");
+// console.log("O número digitado é: " + numero);
+
+
+
+
+// 1- Crie uma função que exiba uma mensagem simples no console. Escolha a mensagem que será exibida no console.log.
+
+// function exibirMensagem() {
+//        console.log("Olá, mundo!");
+// }
+
+// exibirMensagem();
